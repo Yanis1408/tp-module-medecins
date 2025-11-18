2469cde2f7ba56ae0cbd1efb7ae989b5fc47c7f2
Yanis MOKRANE <ymokrane@turgot-paris.si>
2025-11-18 10:34:17 +0100

    feat(helpers): add convertMedecinToDoctor helper

---
+++ src/app/helpers/convert-medecin-to-doctor.ts
@@ -0,0 +1,14 @@
+import { Medecin } from '../types/medecin.interface';
+import { Doctor } from '../types/doctor.interface';
+
+// Fonction classique (pas fléchée) 
+export function convertMedecinToDoctor(medecin: Medecin): Doctor {
+  return {
+    id: medecin.id,    
+    nom: medecin.nom,  
+    prenom: medecin.prenom,  
+    email: medecin.email,
+    specialite: medecin.specialite,    
+    adresse: medecin.adresse,  
+  };
+}
